81c264e9fc498c43ae48d55cbb71e3c35a563bac
Achraf Boussetta <achraf.boussetta@de.bosch.com>
2023-01-10 18:53:05 +0100

    addede light status

--- light/app/main_swc.c
+++ light/app/main_swc.c
@@ -6,6 +6,15 @@ void light_update_status()
 {
     char door_status= Rte_receiveNotif();
     printf("%c\n", door_status);
+    switch (door_status)
+    {
+        case '1':
+            printf("ligh status : off \n");
+        case '2':
+            printf("ligh status : on \n");
+        default:
+            break;
+    }
 }
 
 int main()
